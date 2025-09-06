4a7452d3343b617d6ecb5c628292222551f9110b
TanviHanda <tanvisona2012@gmail.com>
2025-09-06 17:37:40 +0530

    explore routing concept

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function About(){
+    return(
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+          <h1 className="text-5xl font-bold">About Us Page</h1>
+        </main>
+    )
+}
